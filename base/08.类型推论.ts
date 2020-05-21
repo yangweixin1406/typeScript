939023368887b1de31d4091af137c05a1303b04e
yangweixin1406 <327621797@qq.com>
2020-05-21 16:13:00 +0800

    类型tuilun

---
+++ base/08.类型推论.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: yangweixin
+ * @Date: 2020-05-21 16:06:49
+ * @LastEditTime: 2020-05-21 16:12:32
+ * @LastEditors: Please set LastEditors
+ * @Description: 类型推论
+ * @FilePath: \typeScript\base\08.类型推论.ts
+ */ 
+
+ /**
+  * ts 会对定义变量的类型进行检测
+  */
