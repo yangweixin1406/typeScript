b9b9f4cd69121b2a30e89ed28dccbded9e60f9d3
yangweixin1406 <327621797@qq.com>
2020-05-21 15:10:06 +0800

    泛型

--- base/06.泛型.ts
+++ base/06.泛型.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: yangweixin
  * @Date: 2020-05-20 16:49:10
- * @LastEditTime: 2020-05-20 17:04:48
+ * @LastEditTime: 2020-05-21 09:55:57
  * @LastEditors: Please set LastEditors
  * @Description: Ts学习之泛型
  * @FilePath: \typeScript\base\06.泛型.ts
@@ -22,3 +22,11 @@
  * 使用泛型变量
  */
 
+// 在泛型约束中使用类型参数
+function getProperty(obj: T, key: K) {
+  return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+console.log(getProperty(x, 'a'));
